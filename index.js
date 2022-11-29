936728762eff828ab53e5a2c98a46b3e32875300
wenjia <dwgia027@gmail.com>
2022-11-29 21:18:10 +1100

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.getElementById('main');
+document.body.removeChild(main);
+
+let newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id','victory');
+newHeader.innerHTML = 'Wenjia is the champion';
+document.body.append(newHeader);
